2091b11695c32e778c6bf4a371010244b90812a9
Rajarshi Mandal <rajarshim5@gmail.com>
2025-01-03 22:53:04 +0530

    added function custom delimiter or default , \n

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const calculator = (numstr) => {
+const calculatorCommaSeparated = (numstr) => {
     let arr = []
     numstr.split(',').map(e=>{
         arr.push(Number(e))
@@ -6,4 +6,30 @@ const calculator = (numstr) => {
     const total = arr.reduce((acc, curr) => acc + curr, 0)
     return total
 }
-module.exports = calculator;
+const calculatorNewLineSeparated = (numstr) => {
+    let arr = []
+    numstr.split('\n').map(e=>{
+        arr.push(Number(e))
+    })
+    console.log(arr)
+    const total = arr.reduce((acc, curr) => acc + curr, 0)
+    return total
+}
+const calculatorFinal = (numstr) => {
+    let arr = []
+    const delimiterCheck = numstr.match(/^\/\/(.+)\n/) 
+    console.log(delimiterCheck)
+    let delimiter = /[,\n]/;
+    if (delimiterCheck) {
+        delimiter = new RegExp(`[${delimiterCheck[1]}]`);
+        numstr = numstr.replace(/^\/\/(.+)\n/, '')  
+    }
+    console.log('delimiter', delimiter)
+    numstr.split(delimiter).map(e=>{
+        arr.push(Number(e))
+    })
+    console.log(arr)
+    const total = arr.reduce((acc, curr) => acc + curr, 0)
+    return total
+}
+module.exports = {calculatorCommaSeparated, calculatorNewLineSeparated, calculatorFinal};
--- test/test.js
+++ test/test.js
@@ -1,10 +1,25 @@
-const calculator = require("../index");
+const { calculatorFinal,calculatorCommaSeparated, calculatorNewLineSeparated} = require("../index");
 
 describe("calculator", () => {
+    // test("should return true", () => {
+    //     expect(calculatorCommaSeparated("")).toBe(0);
+    // });
+    // test("should return true", () => {
+    //     expect(calculatorCommaSeparated("1")).toBe(1);
+    // });
+    // test("should return true", () => {
+    //     expect(calculatorCommaSeparated("1,2,3")).toBe(6);
+    // });
+    // test("should return true", () => {
+    //     expect(calculatorNewLineSeparated("1\n2\n3\n9")).toBe(15);
+    // });
     test("should return true", () => {
-        expect(calculator("1,2,3")).toBe(6);
+        expect(calculatorFinal("//p\n1p2p3p9")).toBe(15);
     });
     test("should return true", () => {
-        expect(calculator("1,2,d")).toBe(NaN);
+        expect(calculatorFinal("1,2\n3,15")).toBe(21);
     });
+    // test("should return true", () => {
+    //     expect(calculatorFinal("1\n2\n3\n9")).toBe(15);
+    // });
 });
